8af7a2dd1d979acafff528b97dc190dbce1d5eb2
clandau <so.super@gmail.com>
2018-03-21 14:51:40 -0400

    file structure and routes work

--- app/routes/tracker_routes.js
+++ app/routes/tracker_routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app, db) {
     app.post('/api/exercise/new-user', (req, res) => {
         //add a new user
-        res.send('new user requested');
+        res.send('new user requested'); 
         const user = { username: req.body.username };
     });
 
@@ -10,7 +10,7 @@ module.exports = function(app, db) {
         res.send('exercise add requested');
     });
 
-    app.get('/api/exercise/log?{userId}[&from][&to][&limit]', (req, res) => {
-        res.send('get log requested');
+    app.get('/api/exercise/log/:userId', (req, res) => {
+        res.send({userId: req.params.userId, dates: (req.query.from + ' to ' + req.query.to), limit: req.query.limit});
     }); 
 };
